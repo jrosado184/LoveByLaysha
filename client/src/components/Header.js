f693d9f59bc09078a5db60105378b18d4720abd1
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-02-09 00:47:17 -0500

    changes to header on welcome

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,66 +10,69 @@ const Header = ({ login, dispatch, logIn }) => {
   };
   return (
     <>
-      <div className="flex bg-pink-200 w-100 h-62 shadow-md">
-        <div>
+      <div className="flex bg-pink-200 w-100 h-62 shadow-md flex flex-col">
+        <div className="flex w-full">
           <img
-            className="ml-24 w-44 h-44 rounded-full border-4 border-white"
+            className="ml-6 my-2 w-44 h-44 rounded-full border-4 border-white"
             src={laysha}
             alt=""
           />
-          <h1
+          <h2
             className={
-              login.loggedIn
-                ? "font-girl font-bold ml-20 my-2 text-white border-2 border-pink-200 rounded-full w-fit p-2"
+              login.loggedIn ? "hidden" : "mr-24 my-6 font-girl text-2xl w-2"
+            }
+          >
+            LoveByLaysha
+          </h2>
+        </div>
+        <div
+          className={login.loggedIn ? "font-girl ml-6 text-2xl w-96" : "hidden"}
+        >
+          {login.message}
+        </div>
+        <div
+          className={
+            login.loggedIn
+              ? "sm:flex my-3 font-girl w-80 gap-[.5%]"
+              : "flex my-3 font-girl w-80 gap-1"
+          }
+        >
+          <nav className="h-6 text-1xl border-2 text-center bg-white rounded-full border-pink-300 ml-1 pl-2 pr-2 h-[6%] items-center justify-center flex">
+            <Link to="nails">Nailfies</Link>
+          </nav>
+          <nav className="h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-2 pr-2 h-[6%] items-center justify-center flex">
+            <Link to="book">Book?</Link>
+          </nav>
+          <nav className="h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-1 pr-1 w-20 h-[6%] items-center justify-center flex">
+            <Link to="policies">Policies</Link>
+          </nav>
+          <nav
+            className={
+              !login.loggedIn
+                ? "h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-2 pr-2 w-20 h-[6%] items-center justify-center flex"
                 : "hidden"
             }
           >
-            {login.message}
-          </h1>
-          <div
+            <Link to="policies">Contact</Link>
+          </nav>
+          <nav
             className={
               login.loggedIn
-                ? "sm:flex my-3 font-girl w-80 gap-[.5%]"
-                : "flex my-3 font-girl w-80 gap-1 ml-2"
+                ? "h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 w-28 h-[6%] pl-2 pr-2 items-center justify-center flex"
+                : "hidden"
             }
           >
-            <nav className="h-6 text-1xl border-2 text-center bg-white rounded-full border-pink-300 ml-1 pl-2 pr-2 h-[6%] items-center justify-center flex">
-              <Link to="nails">Nailfies</Link>
-            </nav>
-            <nav className="h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-2 pr-2 h-[6%] items-center justify-center flex">
-              <Link to="book">Book?</Link>
-            </nav>
-            <nav className="h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-1 pr-1 w-20 h-[6%] items-center justify-center flex">
-              <Link to="policies">Policies</Link>
-            </nav>
-            <nav
-              className={
-                !login.loggedIn
-                  ? "h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-2 pr-2 w-20 h-[6%] items-center justify-center flex"
-                  : "hidden"
-              }
-            >
-              <Link to="policies">Contact</Link>
-            </nav>
-            <nav
-              className={
-                login.loggedIn
-                  ? "h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 w-28 h-[6%] pl-2 pr-2 items-center justify-center flex"
-                  : "hidden"
-              }
-            >
-              <Link to="policies">Dashboard</Link>
-            </nav>
-            <nav className="text-1xl border-2 rounded-full text-center bg-white border-pink-300 w-20 h-[6%] pl-2 pr-2 items-center justify-center flex">
-              {!login.loggedIn ? (
-                <Link to="login">Admin</Link>
-              ) : (
-                <Link onClick={handleLogOut} to="/">
-                  Logout
-                </Link>
-              )}
-            </nav>
-          </div>
+            <Link to="policies">Dashboard</Link>
+          </nav>
+          <nav className="text-1xl border-2 rounded-full text-center bg-white border-pink-300 w-20 h-[6%] pl-2 pr-2 items-center justify-center flex">
+            {!login.loggedIn ? (
+              <Link to="login">Admin</Link>
+            ) : (
+              <Link onClick={handleLogOut} to="/">
+                Logout
+              </Link>
+            )}
+          </nav>
         </div>
       </div>
     </>
