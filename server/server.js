c3e90c5048124ff203c464f3618565a48efba6d6
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-03-22 20:43:00 -0400

    reconstruct file upload

--- server/server.js
+++ server/server.js
@@ -1,41 +1,46 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
-const adminRouter = require("./auth/users-router");
-const appointRouter = require("./appointments/appointments-router");
-const deletedRouter = require("./appointments/deleted-router");
-const completedRouter = require("./appointments/completed-router");
+const adminRouter = require('./auth/users-router');
+const appointRouter = require('./appointments/appointments-router');
+const deletedRouter = require('./appointments/deleted-router');
+const completedRouter = require('./appointments/completed-router');
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-server.use(fileUpload());
-server.use("/api/users", adminRouter);
-server.use("/api/appointments", appointRouter);
-server.use("/api/deletedAppointments", deletedRouter);
-server.use("/api/completedAppointments", completedRouter);
+server.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+server.use(express.urlencoded({ extended: true }));
+server.use('/api/users', adminRouter);
+server.use('/api/appointments', appointRouter);
+server.use('/api/deletedAppointments', deletedRouter);
+server.use('/api/completedAppointments', completedRouter);
 
-server.get("/", async (req, res) => {
-  res.send("Welcome to LoveByLayshas server");
+server.get('/', async (req, res) => {
+  res.send('Welcome to LoveByLayshas server');
 });
 
-server.post("/images", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ message: "No file uploaded" });
-  }
+server.post('/image', async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({ status: false, message: 'No Files Uploaded' });
+    } else {
+      const { image } = req.files;
 
-  const image = req.files.image;
+      image.mv('./upload' + image.name);
 
-  image.mv(`./server/images/${image.name}`, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+      res.send({ status: true, message: 'File Uploaded' });
     }
-    res.json({ fileName: image.name, filePath: `/images/${image.name}` });
-  });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = server;
