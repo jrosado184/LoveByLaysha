c7d5b82b42c5c43b1ca7bff807b20a1475818338
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-03-27 21:53:33 -0400

    removed register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Main/Header";
-import Book from "./components/Appointments/Book";
-import Register from "./components/Main/Register";
-import Login from "./components/Main/Login";
-import AppointmentList from "./components/Appointments/AppointmentList";
-import Appointment from "./components/Appointments/Appointment";
-import Uploads from "./components/Appointments/Uploads";
-import Confirm from "./components/Appointments/Confirm";
-import Contact from "./components/Main/Contact";
-import Deleted from "./components/Appointments/Deleted";
-import PrivateRoute from "./utils/PrivateRoute";
-import Nails from "./components/Nails/Nails";
-import Completed from "./components/Completed Appointments/Completed";
-import CompletedAppointmentsList from "./components/Completed Appointments/CompletedAppointmentsList";
-import CompletedInformation from "./components/Completed Appointments/CompletedInformation";
-import CompletedUploads from "./components/Completed Appointments/CompletedUploads";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Main/Header';
+import Book from './components/Appointments/Book';
+import Register from './components/Main/Register';
+import Login from './components/Main/Login';
+import AppointmentList from './components/Appointments/AppointmentList';
+import Appointment from './components/Appointments/Appointment';
+import Uploads from './components/Appointments/Uploads';
+import Confirm from './components/Appointments/Confirm';
+import Contact from './components/Main/Contact';
+import Deleted from './components/Appointments/Deleted';
+import PrivateRoute from './utils/PrivateRoute';
+import Nails from './components/Nails/Nails';
+import Completed from './components/Completed Appointments/Completed';
+import CompletedAppointmentsList from './components/Completed Appointments/CompletedAppointmentsList';
+import CompletedInformation from './components/Completed Appointments/CompletedInformation';
+import CompletedUploads from './components/Completed Appointments/CompletedUploads';
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/nails" element={<Nails />} />
-        <Route path="/book" element={<Book />} />
-        <Route path="/confirm/:id" element={<Confirm />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Nails />} />
+        <Route path='/nails' element={<Nails />} />
+        <Route path='/book' element={<Book />} />
+        <Route path='/confirm/:id' element={<Confirm />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route
-          path="/appointment/:id"
+          path='/appointment/:id'
           element={
-            <div className="sm:w-full flex flex-col desktop:flex-row">
+            <div className='sm:w-full flex flex-col desktop:flex-row'>
               <PrivateRoute>
                 <Appointment />
                 <Uploads />
@@ -41,7 +41,7 @@ const App = () => {
           }
         />
         <Route
-          path="/deleted"
+          path='/deleted'
           element={
             <PrivateRoute>
               <Deleted />
@@ -49,7 +49,7 @@ const App = () => {
           }
         />
         <Route
-          path="/completed"
+          path='/completed'
           element={
             <PrivateRoute>
               <Completed />
@@ -57,7 +57,7 @@ const App = () => {
           }
         />
         <Route
-          path="/completedAppointments"
+          path='/completedAppointments'
           element={
             <PrivateRoute>
               <CompletedAppointmentsList />
@@ -65,9 +65,9 @@ const App = () => {
           }
         />
         <Route
-          path="/completed-information/:id"
+          path='/completed-information/:id'
           element={
-            <div className="sm:w-full flex flex-col desktop:flex-row">
+            <div className='sm:w-full flex flex-col desktop:flex-row'>
               <PrivateRoute>
                 <CompletedInformation />
                 <CompletedUploads />
@@ -76,7 +76,7 @@ const App = () => {
           }
         />
         <Route
-          path="/appointments"
+          path='/appointments'
           element={
             <PrivateRoute>
               <AppointmentList />
