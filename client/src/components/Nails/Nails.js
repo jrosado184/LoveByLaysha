1870e31782a487fb426be5b13f4b73615024aff1
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-05-12 18:28:01 -0400

    separated upload to its own component

--- client/src/components/Nails/Nails.js
+++ client/src/components/Nails/Nails.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import plus from './../../assets/plus.svg';
-import { storage } from './../../firebase/firebase';
+import trash from './../../assets/trash.png';
+import FileUpload from './FileUpload';
+import { storage } from '../../firebase/firebase';
 import {
   ref,
   uploadBytes,
@@ -9,14 +10,12 @@ import {
   getDownloadURL,
   deleteObject,
 } from 'firebase/storage';
-import trash from './../../assets/trash.png';
 
 const Nails = (logIn) => {
   const [token, setToken] = useState(null);
-  const [image, setImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState([]);
   const [removeImage, setRemoveImage] = useState(false);
-
+  const [imageUrl, setImageUrl] = useState([]);
+  const [image, setImage] = useState(null);
   const allImageRef = ref(storage, 'nails/');
 
   const handleImage = () => {
@@ -28,6 +27,16 @@ const Nails = (logIn) => {
       });
     });
   };
+
+  useEffect(() => {
+    listAll(allImageRef).then((res) => {
+      res.items.map((item) =>
+        getDownloadURL(item).then((url) => {
+          setImageUrl((prev) => [...prev, url]);
+        })
+      );
+    });
+  }, []);
 
   const handleDeleteImage = (url) => {
     const imageName = ref(storage, url);
@@ -41,29 +50,19 @@ const Nails = (logIn) => {
     setToken(localStorage.getItem('token'));
   }, [logIn]);
 
-  useEffect(() => {
-    listAll(allImageRef).then((res) => {
-      res.items.map((item) =>
-        getDownloadURL(item).then((url) => {
-          setImageUrl((prev) => [...prev, url]);
-        })
-      );
-    });
-  }, []);
-
   return (
     <>
       {token && (
         <div className='flex flex-col items-end w-full h-14'>
           <button
             onClick={() => setRemoveImage(!removeImage)}
-            className='mr-4 my-6 w-16 h-8 bg-pink-200 border border-pink-500 text-pink-800 shadow-sm rounded-sm'
+            className='mr-7 my-6 w-16 h-8 bg-pink-200 border border-pink-500 text-pink-800 shadow-sm rounded-sm'
           >
             {removeImage ? 'Finish' : 'Delete'}
           </button>
         </div>
       )}
-      <div className='w-full h-full flex flex-wrap justify-center gap-6 py-2'>
+      <div className='w-full h-full flex flex-wrap justify-center gap-6 py-4 pl-6 pr-4'>
         {imageUrl.map((nailData) => {
           return (
             <div className='nail-container w-[45%] h-72 my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full'>
@@ -86,24 +85,7 @@ const Nails = (logIn) => {
             </div>
           );
         })}
-        {token && (
-          <label className='w-[50%] h-72 flex flex-col items-center justify-center my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full cursor-pointer'>
-            <div className='flex flex-col items-center justify-center'>
-              <img className='w-20' src={plus} alt='plus' />
-              <input
-                type='file'
-                className='custom-file-input'
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-              <button
-                onClick={handleImage}
-                className='my-6 w-32 h-8 bg-pink-200 border border-pink-500 text-pink-800 shadow-sm rounded-sm'
-              >
-                Upload Image
-              </button>
-            </div>
-          </label>
-        )}
+        {token && <FileUpload setImage={setImage} handleImage={handleImage} />}
       </div>
     </>
   );
