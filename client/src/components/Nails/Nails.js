6c32041e7ba97bac169c5511717f4271ae9e6d2b
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-05-11 22:50:36 -0400

    added remove image func with firebase

--- client/src/components/Nails/Nails.js
+++ client/src/components/Nails/Nails.js
@@ -2,23 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import plus from './../../assets/plus.svg';
 import { storage } from './../../firebase/firebase';
-import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
-import { v4 } from 'uuid';
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+} from 'firebase/storage';
+import trash from './../../assets/trash.png';
 
 const Nails = (logIn) => {
   const [token, setToken] = useState(null);
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState([]);
+  const [removeImage, setRemoveImage] = useState(false);
 
   const allImageRef = ref(storage, 'nails/');
 
   const handleImage = () => {
     if (image === null) return;
-    const imageRef = ref(storage, `nails/${image.name + v4()}`);
+    const imageRef = ref(storage, `nails/${image.name}`);
     uploadBytes(imageRef, image).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
         setImageUrl((prev) => [...prev, url]);
       });
+    });
+  };
+
+  const handleDeleteImage = (url) => {
+    const imageName = ref(storage, url);
+    const imageRef = ref(storage, `nails/${imageName.name}`);
+    deleteObject(imageRef).then(() => {
+      setImageUrl(imageUrl.filter((img) => img !== url));
     });
   };
 
@@ -37,36 +52,60 @@ const Nails = (logIn) => {
   }, []);
 
   return (
-    <div className='w-full h-full flex flex-wrap justify-center gap-6 py-2'>
-      {imageUrl.map((nailData) => {
-        return (
-          <img
-            key={v4()}
-            className='sm:w-[45%] h-72 my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full'
-            src={nailData}
-            alt=''
-          />
-        );
-      })}
+    <>
       {token && (
-        <label className='w-[50%] h-72 flex flex-col items-center justify-center my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full cursor-pointer'>
-          <div className='flex flex-col items-center justify-center'>
-            <img className='w-20' src={plus} alt='plus' />
-            <input
-              type='file'
-              className='custom-file-input'
-              onChange={(e) => setImage(e.target.files[0])}
-            />
-            <button
-              onClick={handleImage}
-              className='w-32 h-8 border border-black rounded-full bg-pink-400 text-white'
-            >
-              Upload Image
-            </button>
-          </div>
-        </label>
+        <div className='flex flex-col items-end w-full'>
+          <button
+            onClick={() => setRemoveImage(!removeImage)}
+            className='mr-10 my-6 w-24 h-10 mr-6 bg-pink-200 border border-pink-500 text-pink-800 shadow-sm rounded-sm'
+          >
+            {removeImage ? 'Finish' : 'Delete'}
+          </button>
+        </div>
       )}
-    </div>
+      <div className='w-full h-full flex flex-wrap justify-center gap-6 py-2'>
+        {imageUrl.map((nailData) => {
+          return (
+            <div className='nail-container w-[45%] h-72 my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full'>
+              <div className='delete-nail-con'>
+                {removeImage && (
+                  <button
+                    onClick={() => handleDeleteImage(nailData)}
+                    className='delete-nail-btn'
+                  >
+                    <img src={trash} alt='trash' />
+                  </button>
+                )}
+              </div>
+              <img
+                key={nailData}
+                className='h-full w-full'
+                src={nailData}
+                alt=''
+              />
+            </div>
+          );
+        })}
+        {token && (
+          <label className='w-[50%] h-72 flex flex-col items-center justify-center my-2 border-2 border-gray-400 shadow-md rounded-md sm2:w-40 h-full lg:w-[13%] h-full cursor-pointer'>
+            <div className='flex flex-col items-center justify-center'>
+              <img className='w-20' src={plus} alt='plus' />
+              <input
+                type='file'
+                className='custom-file-input'
+                onChange={(e) => setImage(e.target.files[0])}
+              />
+              <button
+                onClick={handleImage}
+                className='w-32 h-8 border border-black rounded-full bg-pink-400 text-white'
+              >
+                Upload Image
+              </button>
+            </div>
+          </label>
+        )}
+      </div>
+    </>
   );
 };
 
