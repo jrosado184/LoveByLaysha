f966d5b65dbd79d634febefb9e8015e77800525f
Javier Rosado <Javy@users-MacBook-Pro.local>
2022-05-31 00:13:28 -0400

    fixed nav height and contact form spacing

--- client/src/components/Main/Contact.js
+++ client/src/components/Main/Contact.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import instagram from '../../assets/instagram.svg';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import instagram from "../../assets/instagram.svg";
 
 export const Contact = () => {
   const form = useRef();
@@ -30,13 +30,13 @@ export const Contact = () => {
       <div className='flex flex-col lg:flex-row w-[100%] h-full items-center'>
         <div className='w-[100%] lg:w-[50%]'>
           <form
-            className='flex flex-col justify-center items-center lg:w-[100%]'
+            className='flex my-4 flex-col justify-center items-center lg:w-[100%]'
             ref={form}
           >
-            <h1 className='my-2'>Contact Me</h1>
+            <h1 className='my-2 font-semibold'>Contact Me</h1>
             <input
               name='user_name'
-              className='pl-3 my-6 w-[88%] h-10 rounded-full border-2 border-pink-300 shadow-md md:ml-6'
+              className='pl-3 my-4 w-[88%] h-10 rounded-full border-2 border-pink-300 shadow-md md:ml-6'
               placeholder='Name'
             />
             <input
@@ -47,11 +47,11 @@ export const Contact = () => {
             />
             <textarea
               placeholder='Message'
-              className='pl-3 my-6 w-[88%] pb-24 py-3 rounded-lg border-2 border-pink-300 shadow-md md:ml-6'
+              className='pl-3 my-4 w-[88%] pb-24 py-3 rounded-lg border-2 border-pink-300 shadow-md md:ml-6'
               name='message'
             />
             <input
-              className='w-20 h-8 my-3 border-2 border-pink-300 bg-pink-100 text-rose-500 rounded-full sm2:ml-[70%] md:ml-[74%] lg:ml-[79%]'
+              className='w-20 h-8 my-4 border-2 border-pink-300 bg-pink-100 text-rose-500 rounded-full sm2:ml-[70%] md:ml-[74%] lg:ml-[79%]'
               type='button'
               value='Send'
               onClick={sendEmail}
--- client/src/components/Main/Header.js
+++ client/src/components/Main/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import laysha from '../../assets/laysha.jpg';
-import user from '../../assets/user.png';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { loggedOut } from '../../redux/actions/login_actions';
+import React from "react";
+import laysha from "../../assets/laysha.jpg";
+import user from "../../assets/user.png";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { loggedOut } from "../../redux/actions/login_actions";
 
 const Header = ({ dispatch }) => {
   const handleLogOut = () => {
@@ -14,7 +14,7 @@ const Header = ({ dispatch }) => {
     <>
       <div className='bg-pink-200 w-full h-46 shadow-md flex flex-col'>
         <div className='w-full'>
-          {localStorage.getItem('token') ? (
+          {localStorage.getItem("token") ? (
             <div className='container'>
               <img
                 className='user-image ml-6 my-4 w-44 h-44 rounded-full border-4 border-white md:w-44'
@@ -30,15 +30,15 @@ const Header = ({ dispatch }) => {
             />
           )}
           <div className='pl-14 py-1 text-2xl w-full'>
-            <p className='font-light'>{localStorage.getItem('message')}</p>
+            <p className='font-light'>{localStorage.getItem("message")}</p>
           </div>
         </div>
       </div>
       <div
         className={
-          localStorage.getItem('token')
-            ? 'bg-pink-200 border-b border-pink-500 flex text-md w-full gap-1 items-end justify-end pr-[3.4%] py-2 md:pr-4'
-            : 'bg-pink-200 border-b border-pink-500 flex text-md w-full gap-1 items-end justify-end pr-[2.6%] py-2 md:pr-4'
+          localStorage.getItem("token")
+            ? "bg-pink-200 border-b border-pink-500 flex text-md w-full gap-1 items-end justify-end pr-[3.4%] py-2 md:pr-4"
+            : "bg-pink-200 border-b border-pink-500 flex text-md w-full gap-1 items-end justify-end pr-[2.6%] py-2 md:pr-4"
         }
       >
         <nav className='ml-2 text-1xl border-2 text-center bg-white rounded-full border-pink-300 pl-3 pr-3 h-[6%] items-center justify-center flex'>
@@ -46,51 +46,51 @@ const Header = ({ dispatch }) => {
         </nav>
         <nav
           className={
-            !localStorage.getItem('token')
-              ? 'text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 h-[6%] items-center justify-center flex'
-              : 'hidden'
+            !localStorage.getItem("token")
+              ? "text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 h-[6%] items-center justify-center flex"
+              : "hidden"
           }
         >
           <Link to='book'>Book</Link>
         </nav>
         <nav
           className={
-            localStorage.getItem('token')
-              ? 'hidden'
-              : 'h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-20 h-[6%] items-center justify-center flex'
+            localStorage.getItem("token")
+              ? "hidden"
+              : "h-[6%] text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-20 h-[6%] items-center justify-center flex"
           }
         >
           <Link to='policies'>Policies</Link>
         </nav>
         <nav
           className={
-            localStorage.getItem('token')
-              ? 'h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-24 h-[6%] items-center justify-center flex'
-              : 'hidden'
+            localStorage.getItem("token")
+              ? "h-[6%] text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-24 h-[6%] items-center justify-center flex"
+              : "hidden"
           }
         >
           <Link to='schedule'>Schedule</Link>
         </nav>
         <nav
           className={
-            !localStorage.getItem('token')
-              ? 'h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-20 h-[6%] items-center justify-center flex'
-              : 'hidden'
+            !localStorage.getItem("token")
+              ? "h-[6%] text-1xl border-2 rounded-full text-center bg-white border-pink-300 pl-3 pr-3 w-20 h-[6%] items-center justify-center flex"
+              : "hidden"
           }
         >
           <Link to='contact'>Contact</Link>
         </nav>
         <nav
           className={
-            localStorage.getItem('token')
-              ? 'h-6 text-1xl border-2 rounded-full text-center bg-white border-pink-300 w- h-[6%] pl-2 pr-2 items-center justify-center flex'
-              : 'hidden'
+            localStorage.getItem("token")
+              ? "h-[6%] text-1xl border-2 rounded-full text-center bg-white border-pink-300 w- h-[6%] pl-2 pr-2 items-center justify-center flex"
+              : "hidden"
           }
         >
           <Link to='appointments'>Appointments</Link>
         </nav>
         <nav className='text-1xl border-2 rounded-full text-center bg-white border-pink-300 w-20 h-[6%] pl-3 pr-3 items-center justify-center flex'>
-          {!localStorage.getItem('token') ? (
+          {!localStorage.getItem("token") ? (
             <Link to='login'>Admin</Link>
           ) : (
             <Link onClick={handleLogOut} to='/'>
