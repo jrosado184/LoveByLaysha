b3bd2bb9e2605c46797b84377578b11843b20e07
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-06-08 00:08:05 -0400

    test

--- client/src/components/Mobile/Settings.js
+++ client/src/components/Mobile/Settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Settings = () => {
   return (
-    <div className='w-full h-96 flex justify-center items-end'>Settings</div>
+    <div className='w-full h-96 flex justify-center items-center'>Settings</div>
   );
 };
 
