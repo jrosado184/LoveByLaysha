c134b039fff3473eaa0faf2a405bf38ba7afd079
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-06-07 23:59:23 -0400

    fix

--- client/src/components/Mobile/Settings.js
+++ client/src/components/Mobile/Settings.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className='w-full h-96 flex justify-center items-end'>Settings</div>
+  );
 };
 
 export default Settings;
