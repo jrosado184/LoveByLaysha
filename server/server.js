c142d2f4baf489c65fc544afea569ec899e855ed
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-07-16 23:16:03 -0400

    added cache control

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const completedRouter = require('./appointments/completed-router');
 const disabledDays = require('./appointments/disabled days/disabled-router');
 const disabledTimes = require('./appointments/disabled_times/disabled_times_router');
 
+const path = require('path');
+
+const options = {
+  etag: true,
+  maxAge: 360000,
+  redirect: true,
+};
+
 const server = express();
 server.use(express.json());
 server.use(helmet());
@@ -17,5 +25,12 @@ server.use('/api/appointments', appointRouter);
 server.use('/api/completedAppointments', completedRouter);
 server.use('/api/disabledDays', disabledDays);
 server.use('/api/disabledTimes', disabledTimes);
+server.use(
+  express.static(path.join(__dirname, '..', 'client', 'public'), options)
+);
+
+server.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'favicon.ico'));
+});
 
 module.exports = server;
