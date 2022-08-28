ff9f2bdf8a0c42191745875184fa7b35d4d6c4be
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-08-27 23:37:41 -0400

    fixed loading skeleton for nial images

--- client/src/components/Nails/skeletons/NailSkeleton.js
+++ client/src/components/Nails/skeletons/NailSkeleton.js
@@ -5,7 +5,28 @@ const NailSkeleton = ({ cards }) => {
   return (
     <div className='h-full'>
       <div className='w-full h-full flex flex-wrap justify-center  gap-6 pl-6 pb-20 pr-4 py-2 desktop:py-[1.7%]'>
-        <div className='w-full flex justify-end pr-6 my-1'></div>
+        <div
+          className={
+            localStorage.getItem('token')
+              ? 'w-full flex justify-end desktop:hidden'
+              : 'hidden'
+          }
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-8 h-8 text-pink-900 dark:text-white'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+            />
+          </svg>
+        </div>
         {Array(cards)
           .fill(0)
           .map((e, i) => (
