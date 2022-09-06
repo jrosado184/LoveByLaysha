de376761cb87b4ca25a2ed0907f97dc997d88ae2
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-09-06 13:06:16 -0400

    fixed reschedule endpoint

--- server/appointments/reschedule/reschedule-router.js
+++ server/appointments/reschedule/reschedule-router.js
@@ -1,14 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Reschedule = require('./reschedule-model');
 
-router.get("/", (req, res, next) => {
-  res.json({ message: "hello" });
+router.get('/', (req, res, next) => {
+  const { client_name, confirmation } = req.body;
+
+  Reschedule.findBy(confirmation, client_name)
+    .then((reschedule) => {
+      res.json(reschedule);
+    })
+    .catch(next);
 });
 
 router.use((err, req, res, next) => {
   res
     .status(500)
-    .json({ message: err.message, custom: "error in the reschedule router" });
+    .json({ message: err.message, custom: 'error in the reschedule router' });
 });
 
 module.exports = router;
