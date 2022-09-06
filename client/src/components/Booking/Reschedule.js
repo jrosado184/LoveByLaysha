4166b03772b9f93e53014236cbeb1a178d10e1d1
Javier Rosado <javierrosado184@gmail.com>
2022-09-05 20:42:01 -0400

    testing reschedule endpoint

--- client/src/components/Booking/Reschedule.js
+++ client/src/components/Booking/Reschedule.js
@@ -17,7 +17,7 @@ const Reschedule = () => {
   const handleRequest = (e) => {
     e.preventDefault();
     axiosWithAuth()
-      .get("/api/reschedule")
+      .get("/api/completedAppointments")
       .then((res) => {
         console.log(res);
       })
--- server/appointments/completed-router.js
+++ server/appointments/completed-router.js
@@ -1,24 +1,24 @@
-const router = require('express').Router();
-const Completed = require('./completed-model');
-const { checkId } = require('./completed-middleware');
+const router = require("express").Router();
+const Completed = require("./completed-model");
+const Reschedule = require("./reschedule/reschedule-model");
+const { checkId } = require("./completed-middleware");
 
-router.get('/', (req, res, next) => {
-  Completed.findAll()
-    .then((completed) => {
-      res.json(completed);
-    })
-    .catch(next);
+router.get("/", (req, res, next) => {
+  const { client_name, confirmation } = req.body;
+  Reschedule.findBy(client_name, confirmation).then((res) => {
+    res.json(res);
+  });
 });
 
-router.get('/:id', checkId, (req, res, next) => {
-  Completed.findById(req.params.id)
-    .then((appoint) => {
-      res.json(appoint);
-    })
-    .catch(next);
-});
+// router.get('/:id', checkId, (req, res, next) => {
+//   Completed.findById(req.params.id)
+//     .then((appoint) => {
+//       res.json(appoint);
+//     })
+//     .catch(next);
+// });
 
-router.delete('/:id', checkId, (req, res, next) => {
+router.delete("/:id", checkId, (req, res, next) => {
   Completed.remove(req.params.id)
     .then((completed) => {
       res.json(completed);
@@ -29,7 +29,7 @@ router.delete('/:id', checkId, (req, res, next) => {
 router.use = (err, req, res, next) => {
   res.status(500).json({
     message: err.message,
-    custom: 'There was an error in the completed appointments router',
+    custom: "There was an error in the completed appointments router",
   });
 };
 
