a07a7dd5a369226632236e8608308ed49a1c70d0
Javier Rosado <javierrosado184@gmail.com>
2022-12-11 22:04:43 -0500

    test twilio

--- server/appointments/appointments-router.js
+++ server/appointments/appointments-router.js
@@ -5,9 +5,9 @@ const {
   checkId,
   checkExists,
 } = require("./appointments-middleware");
+const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilio = require("twilio");
 
 const client = new twilio(accountSid, authToken);
 
