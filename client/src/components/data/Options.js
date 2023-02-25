6b775cff3c043e851a9078912983d7c6c73adf9c
Javier Rosado <javierrosado184@gmail.com>
2023-02-24 23:23:44 -0500

    wrote an algorithm to replace brute force of booking times that are unavailable or already booked

--- client/src/components/data/Options.js
+++ client/src/components/data/Options.js
@@ -1,316 +1,112 @@
 export const Options = ({ disabledTimes }) => {
   const times = [
-    { time: '10:00 AM' },
-    { time: '10:30 AM' },
-    { time: '11:00 AM' },
-    { time: '11:30 AM' },
-    { time: '12:00 PM' },
-    { time: '12:30 PM' },
-    { time: '1:00 PM' },
-    { time: '1:30 PM' },
-    { time: '2:00 PM' },
-    { time: '2:30 PM' },
-    { time: '3:00 PM' },
-    { time: '3:30 PM' },
-    { time: '4:00 PM' },
-    { time: '4:30 PM' },
-    { time: '5:00 PM' },
-    { time: '5:30 PM' },
-    { time: '6:00 PM' },
+    "10:00 AM",
+    "10:30 AM",
+    "11:00 AM",
+    "11:30 AM",
+    "12:00 PM",
+    "12:30 PM",
+    "1:00 PM",
+    "1:30 PM",
+    "2:00 PM",
+    "2:30 PM",
+    "3:00 PM",
+    "3:30 PM",
+    "4:00 PM",
+    "4:30 PM",
+    "5:00 PM",
+    "5:30 PM",
+    "6:00 PM",
   ];
+
+  const isTimeUnavailable = (index) => {
+    let map = {};
+    let queue = [];
+
+    disabledTimes.map((val) => queue.push(val));
+
+    while (queue.length) {
+      let left =
+        times.indexOf(queue[0]) - 5 < 0 ? 0 : times.indexOf(queue[0]) - 5;
+      let right =
+        times.indexOf(queue[0]) + 5 > times.length - 1
+          ? times.length - 1
+          : times.indexOf(queue[0]) + 5;
+      for (let i = left; i <= right; i++) {
+        map[i] = times[i];
+      }
+      queue.shift();
+    }
+    return index in map;
+  };
 
   return (
     <>
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time)
-        }
-        value='10:00 AM'
-      >
+      <option disabled={isTimeUnavailable(0)} value='10:00 AM'>
         10:00 AM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time)
-        }
-        value='10:30 AM'
-      >
+      <option disabled={isTimeUnavailable(1)} value='10:30 AM'>
         10:30 AM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time)
-        }
-        value='11:00 AM'
-      >
+      <option disabled={isTimeUnavailable(2)} value='11:00 AM'>
         11:00 AM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time)
-        }
-        value='11:30 AM'
-      >
+      <option disabled={isTimeUnavailable(3)} value='11:30 AM'>
         11:30 AM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time)
-        }
-        value='12:00 PM'
-      >
+      <option disabled={isTimeUnavailable(4)} value='12:00 PM'>
         12:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time)
-        }
-        value='12:30 PM'
-      >
+      <option disabled={isTimeUnavailable(5)} value='12:30 PM'>
         12:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[0].time) ||
-          disabledTimes.includes(times[1].time) ||
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time)
-        }
-        value='1:00 PM'
-      >
+      <option disabled={isTimeUnavailable(6)} value='1:00 PM'>
         1:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[2].time) ||
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time)
-        }
-        value='1:30 PM'
-      >
+      <option disabled={isTimeUnavailable(7)} value='1:30 PM'>
         1:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[3].time) ||
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time)
-        }
-        value='2:00 PM'
-      >
+      <option disabled={isTimeUnavailable(8)} value='2:00 PM'>
         2:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[4].time) ||
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time)
-        }
-        value='2:30 PM'
-      >
+      <option disabled={isTimeUnavailable(9)} value='2:30 PM'>
         2:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[5].time) ||
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time)
-        }
-        value='3:00 PM'
-      >
+      <option disabled={isTimeUnavailable(10)} value='3:00 PM'>
         3:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[6].time) ||
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='3:30 PM'
-      >
+      <option disabled={isTimeUnavailable(11)} value='3:30 PM'>
         3:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[7].time) ||
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='4:00 PM'
-      >
+      <option disabled={isTimeUnavailable(12)} value='4:00 PM'>
         4:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[8].time) ||
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='4:30 PM'
-      >
+      <option disabled={isTimeUnavailable(13)} value='4:30 PM'>
         4:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[9].time) ||
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='5:00 PM'
-      >
+      <option disabled={isTimeUnavailable(14)} value='5:00 PM'>
         5:00 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[10].time) ||
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='5:30 PM'
-      >
+      <option disabled={isTimeUnavailable(15)} value='5:30 PM'>
         5:30 PM
       </option>
       ;
-      <option
-        disabled={
-          disabledTimes.includes(times[11].time) ||
-          disabledTimes.includes(times[12].time) ||
-          disabledTimes.includes(times[13].time) ||
-          disabledTimes.includes(times[14].time) ||
-          disabledTimes.includes(times[15].time) ||
-          disabledTimes.includes(times[16].time)
-        }
-        value='6:00 PM'
-      >
+      <option disabled={isTimeUnavailable(16)} value='6:00 PM'>
         6:00 PM
       </option>
       ;
@@ -319,21 +115,21 @@ export const Options = ({ disabledTimes }) => {
 };
 
 export const styles = [
-  'Shorties Full Set',
-  'Medium Full Set',
-  'Long Full Set',
-  'XL Full Set',
-  'XXL Full Set',
-  'Extendo Full Set',
-  'Freestyle',
+  "Shorties Full Set",
+  "Medium Full Set",
+  "Long Full Set",
+  "XL Full Set",
+  "XXL Full Set",
+  "Extendo Full Set",
+  "Freestyle",
 ];
 
 export const refillSet = [
-  'Shorties Full Set',
-  'Medium Full Set',
-  'Long Full Set',
-  'XL Full Set',
-  'XXL Full Set',
-  'Extendo Full Set',
-  'Freestyle',
+  "Shorties Full Set",
+  "Medium Full Set",
+  "Long Full Set",
+  "XL Full Set",
+  "XXL Full Set",
+  "Extendo Full Set",
+  "Freestyle",
 ];
